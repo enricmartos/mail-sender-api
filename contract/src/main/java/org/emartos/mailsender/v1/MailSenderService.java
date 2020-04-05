850671db658df716634eb2a66f51858cf72e0cf7
enricmartos <enric.martos@gmail.com>
2020-04-05 15:43:07 +0200

    create mail-sender-api

--- contract/src/main/java/org/emartos/mailsender/v1/MailSenderService.java
+++ contract/src/main/java/org/emartos/mailsender/v1/MailSenderService.java
@@ -7,14 +7,12 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @RequestMapping(MailSenderServiceApi.MAIL_SENDER_API_PATH + MailSenderServiceApi.VERSION)
 public interface MailSenderService {
 
     @PostMapping("/email")
     void sendEmail(@RequestHeader("apiKey") String apiKey, @RequestParam("from") String from,
-                   @RequestParam("to")List<String> to, @RequestParam("body") String body)
+                   @RequestParam("to") String to, @RequestParam("body") String body)
             throws BadRequestException, MailNotSendException;
 
 }
